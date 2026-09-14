8de67b3d2e14c32e531d63d03ed09796abb7efef
Tsogtbayar <mtsogtbayar24@gmail.com>
2026-09-15 02:03:06 +0800

    Complete README conclusions

--- script_threshold.js
+++ script_threshold.js
@@ -5,8 +5,7 @@ export const options = {
   vus: 30, 
   duration: "1m",
   thresholds: {
-    
-    http_req_duration: ['p(95)<300'], // SLO: p95 < 300ms
+    http_req_duration: ['p(95)<50'], // SLO: p95 < 633ms
     http_req_failed: ['rate<0.01'],   // SLO: error rate < 1%
   },
 };
